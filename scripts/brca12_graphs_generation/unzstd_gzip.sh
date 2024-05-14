67af74ab4c7d91fbf6424c64c86af620a1233783
Francesco Andreace <francesco.andreace@gmail.com>
2024-05-14 18:32:44 +0200

    refreshed ignored files.

--- scripts/brca12_graphs_generation/unzstd_gzip.sh
+++
@@ -1,5 +0,0 @@
-filename=$1
-outfile=$2
-out_filename="${filename%.*}.gz"
-unzstd $1 | gzip > $out_filename
-echo $out_filename >> $outfile
--- scripts/run_scripts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# MAP UNITIGS TO THE REFERENCE
-./map_utgs_to_reference.sh ../data/sequences/chr18.fa ../data/unitigs/unitigs_chr18_masked.fa ../data/processed/unitigs_chr18_masked_position.tsv
-
-# COLOR THE GFA WITH RELATIVE POSITION IN THE CHROMOSOME
-./color_gfa.sh ../data/gfa/unitigs_chr18_masked.gfa ../data/processed/unitigs_chr18_masked_position.tsv ../data/gfa/chr18_masked_positioned.gfa
